c9e8fa5145929336d81a139ec895166bd799d89c
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-22 13:35:30 +0100

    virtualized models , removed _id and __v

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,18 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 const debug = require("debug")("items:database");
 const chalk = require("chalk");
 const mongoose = require("mongoose");
 
 const connectDB = (connectionString) =>
   new Promise((resolve, reject) => {
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        delete ret._id;
+        delete ret.__v;
+      },
+    });
     mongoose.connect(connectionString, (error) => {
       if (error) {
         reject(error);
